76d186a47ab979ce71148f28b6edb788a599036a
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2026-02-13 08:31:06 +0100

    Docstring cleanup

--- src/wmoutils/query.py
+++ src/wmoutils/query.py
@@ -4,6 +4,8 @@ Copyright (c) 2026 MeteoSwiss, contributors listed in AUTHORS
 Distributed under the terms of the BSD 3-Clause License.
 
 SPDX-License-Identifier: BSD-3-Clause
+
+Module contains: functions to query external APIs, e.g. WDQMS and OSCAR/Surface.
 """
 
 # Import from Python
